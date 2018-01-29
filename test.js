8c7e976362a57f96dbffdd0799086d0f480b6128
Victor Sarker <victorsarker@NN-34739.local>
2018-01-29 17:25:34 +0000

    rooms work now

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-for (let i = 2; i < 34; i += 1) {
-  const rounds = Math.ceil(Math.log2(i)) + Math.ceil(Math.log2(Math.log2(i)));
-  const placing = 
-  console.log(`entrants:${i}`);
-  console.log(`rounds: ${rounds}\n`);
-  console.log()
+const readlineSync = require('readline-sync');
+
+const options = ['Open Box', 'Continue On'];
+const index = readlineSync.keyInSelect(options, 'What do you do?');
+
+if (index === 0) {
+  console.log('You open the box.');
+} else if (index === 1) {
+  console.log('You ignore the box and carry on.');
+} else {
+  console.log('You give up and die.');
 }
